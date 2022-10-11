c4ca56324aff908fbd23def83be1b6a1afacd41b
asifMuzammil <m.asif.muzammil@gmail.com>
2022-10-12 01:41:54 +0500

    live class end

--- live.js
+++ live.js
@@ -1,4 +1,5 @@
-let age = Number (prompt("How old Are You"))//Number is defing typeof data
+let age = parseInt (prompt("How old Are You"))//Number is defing typeof data
+//paseInt and parseFloat 
 let bye = String("Your are not in eldgiable for class")//string is change type of data
 console.log(typeof age)
 console.log(typeof bye)
@@ -7,3 +8,24 @@ if (age === 25 ){
     console.log("Well Come")
 }
 else console.log(bye)
+let value1= 4;
+let value2= 5;
+let value3= 6;
+console.log(value1+value3)
+console.log(value1/value3)
+console.log(value3*value1)
+console.log(value2%value3)
+console.log(value1-value3)
+// for check booleans
+// comparison operators
+
+let n= 6;
+console.log(n == 6)
+console.log(n === 6)
+console.log(n != 6)
+console.log(n >= 6)
+console.log(n <= 6)
+console.log(n == 6)
+console.log(n > 6)
+console.log(n < 6)
+console.log(n = 0)
